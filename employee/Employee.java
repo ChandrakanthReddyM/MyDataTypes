811304f93e69f92d4926d6db88f4f87be4904fdb
ChandrakanthReddyM <ChandrakanthReddyM@users.noreply.github.com>
2018-09-05 15:52:04 +0530

    Initial commit

---
+++ employee/Employee.java
@@ -0,0 +1,19 @@
+package mydatatypes.employee;
+public class Employee {
+	int employeeID;
+	char []name;
+	char []emailID;
+	long phoneNumber;
+	char []position;
+	int age;
+	double salary;
+	public void work()
+	{
+	}
+	public void updateWork()
+	{
+	}
+	public Employee()
+	{
+	}
+}
---
+++ entertainment/Movie.java
@@ -0,0 +1,23 @@
+package mydatatypes.entertainment;
+public class Movie {
+		// TODO Auto-generated method stub
+		char []movieName;
+		char []director;
+		char []hero;
+		char []producer;
+		char []heroine;
+		int noOfSong;
+		double budget;
+		double duration;
+		public void action()
+		{
+		}
+		public void dance()
+		{
+		}
+		public Movie()
+		{
+		}
+	}
+
+
